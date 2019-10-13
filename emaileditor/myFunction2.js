ae27b3cad4fa2a35df8421afa988b476e35d0e89
Jaewan <31329026+canshot@users.noreply.github.com>
2019-10-12 22:31:08 -0400

    u

---
+++ emaileditor/myFunction2.js
@@ -0,0 +1,5 @@
+function myFunction2(){
+    var btn = '<button class="button" style="margin-left:920px;" type="submit">next</button>';
+    document.getElementById("demo2").innerHTML = btn;
+    document.getElementById("demo2").action = "https://canshot.github.io/attack/t1/combination/c5.html";
+  }
